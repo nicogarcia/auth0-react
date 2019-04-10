74aa69116883908b50657af15d41f9e560ca2e63
Nicolas <nicolas.garcia@auth0.com>
2019-04-10 11:20:23 -0300

    Inline CLI install instructions

--- scripts/install_cli.sh
+++ scripts/install_cli.sh
@@ -1,3 +1,2 @@
 # Install Auth0 CLI
-curl https://auth0-cli.nicogarcia.now.sh/node8-macos/auth0 -o auth0
-chmod +x ./auth0
+curl https://auth0-cli.nicogarcia.now.sh/node8-macos/auth0 -o auth0 && chmod +x ./auth0
