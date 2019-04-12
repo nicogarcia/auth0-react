9eb2d583ed331cbbc396297c1c9c53ac624e07d8
Nicolas <nicolas.garcia@auth0.com>
2019-04-12 19:11:11 -0300

    Update cli install instructions

--- scripts/install_cli.sh
+++ scripts/install_cli.sh
@@ -1,2 +1,5 @@
 # Install Auth0 CLI
-curl https://auth0-cli.nicogarcia.now.sh/node8-macos/auth0 -o auth0 && chmod +x ./auth0
+curl https://auth0-cli.nicogarcia.now.sh/node8-macos/auth0 -o auth0
+
+# Add executable permission
+chmod +x ./auth0
